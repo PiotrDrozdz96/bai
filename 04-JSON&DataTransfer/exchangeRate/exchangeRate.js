6a26cda004df7333dd6c6556c5a69342d2db093e
Piotr Drożdż <piotr.drozdz0@op.pl>
2020-03-20 18:34:15 +0100

    fix: filtered rates in 04-exchangeRate

--- 04-JSON&DataTransfer/exchangeRate/exchangeRate.js
+++ 04-JSON&DataTransfer/exchangeRate/exchangeRate.js
@@ -3,6 +3,11 @@ xhr.onreadystatechange = display;
 xhr.open('GET', 'http://api.nbp.pl/api/exchangerates/tables/A/?format=json');
 xhr.send();
 
+const get = (array, path, equalities) => {
+  const collection = {};
+  array.forEach((element) => { collection[element[path]] = element });
+  return equalities.map((equality) => collection[equality]);
+};
 
 const createRow = (columns, cellTag = 'td') => `
   <tr>${columns.map((column) => `<${cellTag}>${column}</${cellTag}>`).join('')}</tr>
@@ -14,11 +19,12 @@ function display(){
   if(readyState === 4 && status === 200) {
     const [{ rates }] = JSON.parse(response);
     const keys = Object.keys(rates[0]);
+    const filteredRates = get(rates, 'code', ['EUR', 'USD', 'GBP']);
     const table = document.createElement('table');
 
     table.innerHTML = `
       ${createRow(keys, 'th')}
-      ${rates.map((rate) => (
+      ${filteredRates.map((rate) => (
         createRow(keys.map((key) => rate[key]))
       )).join('')}
     `;
